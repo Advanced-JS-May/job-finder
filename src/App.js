9661acd86de720f769c61c3cd3f5ceaab1f4b43b
Gor <gor.sharoyan95@gmail.com>
2020-10-13 02:14:24 +0400

    �� ci(CompanyInfo): created companyInfo form /firebase DB/firebase DB POST fetches

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 /* import AuthenticatedApp from './components/AuthenticatedApp/AuthenticatedApp'; */
 import UnauthenticatedApp from './components/UnauthenticatedApp/Unauthenticated';
+import CompanyInfo from './pages/CompanyInfo/CompanyInfo';
 
 function App() {
   return (
     <div className="App">
       <header>
         <UnauthenticatedApp />
+        <CompanyInfo />
       </header>
     </div>
   );
---
+++ src/services/company.js
@@ -0,0 +1,31 @@
+  
+import { database } from "../libraries/firebase";
+
+// function to POST information about company to DB 
+export  function createCompany (company) {
+  return database.ref('companies/' + company.id).set(company);
+}
+
+export function getCompanyById(id) {
+  return database
+    .ref('/users/' + id)
+    .once('value')
+    .then((snapshot) => snapshot.val());
+}
+
+
+// function to GET information about company from DB
+// export function getCompany (company) {
+//   return database()
+//   .ref('companies/' + company.id)
+//   .once('value')
+//   .then(function(company) {
+//     console.log(company)
+// //   });
+// }
+// export function getUsersById(id) {
+//   return database
+//     .ref('/users/' + id)
+//     .once('value')
+//     .then((snapshot) => snapshot.val());
+// }
