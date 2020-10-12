842a9ffc1c30f45ea44a3c9ad79fbda7bead6ea4
Gor <gor.sharoyan95@gmail.com>
2020-10-12 22:27:15 +0400

    firebase new config

--- src/libraries/firebase.js
+++ src/libraries/firebase.js
@@ -12,14 +12,25 @@ const {
   REACT_APP_APP_ID,
 } = process.env;
 
+
+// const firebaseConfig = {
+//   apiKey: REACT_APP_API_KEY,
+//   authDomain: REACT_APP_AUTH_DOMAIN,
+//   databaseURL: REACT_APP_DATABASE_URL,
+//   projectId: REACT_APP_PROJECT_ID,
+//   storageBucket: REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+//   appId: REACT_APP_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  databaseURL: REACT_APP_DATABASE_URL,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
+  apiKey: "AIzaSyBWQEpAA5MyZ36GsXoyQv_skG4DtPwd1xg",
+  authDomain: "job-seeker-8e38e.firebaseapp.com",
+  databaseURL: "https://job-seeker-8e38e.firebaseio.com",
+  projectId: "job-seeker-8e38e",
+  storageBucket: "job-seeker-8e38e.appspot.com",
+  messagingSenderId: "850523542035",
+  appId: "1:850523542035:web:b39819a16bac28372765e6"
 };
 
 firebase.initializeApp(firebaseConfig);
