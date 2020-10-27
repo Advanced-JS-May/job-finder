6a93060ac99607c2720e7d911d11202d3e453bfa
sabovyan <abovyan.sargis90@gmail.com>
2020-10-27 14:41:22 +0400

    ⚡perf(Routes) : remove unnecessary components

--- src/components/Routes/PublicRoutes/PublicRoutes.js
+++ src/components/Routes/PublicRoutes/PublicRoutes.js
@@ -5,22 +5,15 @@ import SignIn from '../../../pages/SignIn/SignIn';
 import Signup from '../../../pages/Signup/Signup';
 import Home from '../../../pages/Home/Home';
 import Jobs from '../../../pages/Jobs/Jobs';
-import EmployerSignup from '../../../pages/EmployerSignup/EmployerSignup';
 
 function PublicRoutes() {
   return (
     <>
-      <Route path="/">
+      <Route exact path="/">
         <Home />
-      </Route>
-      <Route path="/company/signup">
-        <EmployerSignup />
       </Route>
       <Route path="/signup">
         <Signup />
-      </Route>
-      <Route path="/company/register">
-        <EmployerSignup />
       </Route>
       <Route path="/login">
         <SignIn />
