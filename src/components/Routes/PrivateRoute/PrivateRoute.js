a21e6530f2c91de3ffb02c775e94a8f94df8382d
sabovyan <abovyan.sargis90@gmail.com>
2020-10-27 17:04:27 +0400

    ⚡perf(Router) : solve protected Route issue

--- src/components/Routes/PrivateRoute/PrivateRoute.js
+++ src/components/Routes/PrivateRoute/PrivateRoute.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
-function PrivateRoute({ children, auth, ...rest }) {
-  return <Route>{auth ? children : <Redirect to="/login" />}</Route>;
+function PrivateRoute({ path, auth, children }) {
+  return (
+    <Route path={path}>
+      {auth === null ? (
+        <LinearProgress />
+      ) : auth ? (
+        children
+      ) : (
+        <Redirect to="/login" />
+      )}
+    </Route>
+  );
 }
 
 export default PrivateRoute;
--- src/components/Routes/SwitchRouter/SwitchRouter.js
+++ src/components/Routes/SwitchRouter/SwitchRouter.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
-import PublicRoutes from '../PublicRoutes/PublicRoutes';
-import PrivateRoutes from '../PrivateRoutes/PrivateRoutes';
+import { Switch, Route } from 'react-router-dom';
+
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
+import { useAuth } from '../../../services/authentication';
+
+import SignIn from '../../../pages/SignIn/SignIn';
+import Signup from '../../../pages/Signup/Signup';
+import Home from '../../../pages/Home/Home';
+import Jobs from '../../../pages/Jobs/Jobs';
+import CompanyInfo from '../../../pages/CompanyInfo/CompanyInfo';
+import EmailVerification from '../../../pages/EmailVerification/EmailVerification';
 
 function SwitchRouter() {
+  const { user } = useAuth();
+
   return (
     <Switch>
-      <PublicRoutes />
-      <PrivateRoutes />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/login">
+        <SignIn />
+      </Route>
+      <Route path="/jobs">
+        <Jobs />
+      </Route>
+      <Route path="/email-verification">
+        <EmailVerification />
+      </Route>
+
+      <PrivateRoute auth={user && user.emailVerified} path="/company/create">
+        <CompanyInfo />
+      </PrivateRoute>
     </Switch>
   );
 }
