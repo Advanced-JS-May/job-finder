5f6a9617a9b9340dc53f54621b9e5bde64295976
Tigran515 <tigikarap@yahoo.com>
2020-11-08 13:41:02 +0400

    💚ci(JobTimeline) : render Jobs

--- src/pages/Jobs/Jobs.js
+++ src/pages/Jobs/Jobs.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect , useState } from "react";
+import JobCard from "../../components/JobCard/JobCard";
+import { makeStyles } from "@material-ui/core/styles";
+import {getAllJobs} from "../../services/getAllJobs"
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+  },
+});
 
 function Jobs() {
-  return <h1>Jobs</h1>;
+  const [jobs, setJobs] = useState([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    getAllJobs().then((response) => {
+      setJobs(Object.values(response));
+    });
+  }, []);
+  console.log("out",jobs);
+
+  // useEffect(() => {
+  //   getAllJobs().then((res) => {
+  //     console.log("Res before",res);
+
+  //     setJobs((t)=>{t.push(res)})
+
+  //     console.log("JOB",jobs);
+  //   }
+  //   ); // very bad code 
+
+
+  
+  return (
+    <div className={classes.root}>
+    {jobs.map(({id,position})=>{
+      return(
+        <JobCard jobTitle={position} />
+      )
+    })}
+    </div>
+  );
 }
 
 export default Jobs;
