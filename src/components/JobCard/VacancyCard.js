3169e28eefe2134f5db28cea2dd2c9d56e89f85d
GorSharoyan <gor.sharoyan95@gmail.com>
2021-03-03 20:00:54 +0400

    🎨 Ui (VacancyCard): added new bar to the announcement

--- src/components/JobCard/VacancyCard.js
+++ src/components/JobCard/VacancyCard.js
@@ -11,20 +11,29 @@ import MuiAlert from "@material-ui/lab/Alert";
 import Snackbar from "@material-ui/core/Snackbar";
 import SnackbarContent from "@material-ui/core/SnackbarContent";
 import Grid from "@material-ui/core/Grid";
+import { Category } from "@material-ui/icons";
 
 const useStyles = makeStyles({
+  minibar: {
+    height: "10px",
+  },
   bar: {
     color: "black",
     backgroundColor: "#e6e2d3",
     padding: "15px",
     height: "15px",
   },
+
   section: {
     padding: "15px",
   },
 });
 
 export default function VacancyCard({
+  company,
+  category,
+  address,
+  deadline,
   position,
   responsabilites,
   qualifications,
@@ -46,6 +55,39 @@ export default function VacancyCard({
             <MuiAlert variant="filled" severity="info">
               {position}
             </MuiAlert>
+          </div>
+          <div className={classes.section}>
+            <Grid
+              container
+              direction="row"
+              justify="space-evenly"
+              alignItems="center"
+            >
+              <Card>
+                <SnackbarContent message="Company" />
+                <CardContent className={classes.minibar}>
+                  <p>{company}</p>
+                </CardContent>
+              </Card>
+              <Card>
+                <SnackbarContent message="Category" />
+                <CardContent>
+                  <p>{category}</p>
+                </CardContent>
+              </Card>
+              <Card>
+                <SnackbarContent message="Job Adress" />
+                <CardContent>
+                  <p>{address}</p>
+                </CardContent>
+              </Card>
+              <Card>
+                <SnackbarContent message="Deadline" />
+                <CardContent>
+                  <p>{deadline}</p>
+                </CardContent>
+              </Card>
+            </Grid>
           </div>
           <div className={classes.section}>
             <Card>
